a79ff83860beec2d2bad93ba981ed2ff324fe48c
User <lesu_gtj2@student.itstep.org>
2022-10-23 09:54:01 +0300

    Init

---
+++ quiz.py
@@ -0,0 +1,115 @@
+print("Седьмой дед это-")
+print('а)ИС-4')
+print('б)Hetzer')
+print('в) ИС-7')
+print('г)M-4 Sherman')
+print('д) т-34')
+
+answer1 = input('Ответ: ')
+
+if answer1 == 'в':
+    print(' Ты норм танкист')
+elif answer1 == 'а':
+    print("Иди в танки поиграй!")
+elif answer1 == 'б':
+    print("Иди в танки поиграй!")
+elif answer1 == 'д':
+    print("Иди в танки поиграй!")
+elif answer1 == 'г':
+    print("Иди в танки поиграй!")
+
+print('Бабаха это-')
+print('а) AMX 13b')
+print('б) КВ-2')
+print('в) Grille 15')
+print('г) Churchill 1')
+print('д) FV 215 (183)b')
+
+
+answer2 = input('Ответ: ')
+
+
+if answer2 == 'д':
+    print(' Ты норм танкист')
+elif answer2 == 'а':
+    print("Иди в танки поиграй!")
+elif answer2 == 'б':
+    print("Иди в танки поиграй!")
+elif answer2 == 'в':
+    print("Иди в танки поиграй!")
+elif answer2 == 'г':
+    print("Иди в танки поиграй!")
+
+print('Курица это-')
+print('а) Grille 15')
+print('б) КВ-1С')
+print('в) ЛТГ')
+print('г) АТ-2')
+print('д) M5 Stuart')
+
+answer3 = input('Ответ: ')
+
+if answer3 == 'а':
+    print(' Ты хороший танкист')
+elif answer3 == 'д':
+    print("Иди в танки поиграй!")
+elif answer3 == 'б':
+    print("Иди в танки поиграй!")
+elif answer3 == 'в':
+    print("Иди в танки поиграй!")
+elif answer3 == 'г':
+    print("Иди в танки поиграй!")
+
+
+print('Ветка боли это-')
+print('а) ветка ИС-7')
+print('б) ветка гриля')
+print('в) ветка бабахи')
+print('г) ветка мыши')
+print('д) ветка сосисы')
+
+
+answer4 = input('Ответ: ')
+
+if answer4 == 'в':
+    print(' Ты хороший танкист')
+elif answer4 == 'д':
+    print("Нуб! Сосиса это 6-ка донатная")
+elif answer4 == 'б':
+    print("Иди в танки поиграй!")
+elif answer4 == 'а':
+    print("Иди в танки поиграй!")
+elif answer4 == 'г':
+    print("Иди в танки поиграй!")
+
+print('Китайская пародия это-')
+print('а) т-34')
+print('б) type t-34')
+
+answer5 = input('Ответ: ')
+
+if answer5 == 'б':
+    print(' Ты хороший танкист')
+elif answer5 == 'а':
+    print("Как ты так мог сказать про легенду?!!")
+
+
+print('T-95 это-')
+print('а) черепаха')
+print('б) мышь')
+print('в) бабаха')
+print('г) курица')
+print('д) елка')
+
+answer6 = input('Ответ: ')
+
+if answer6 == 'а':
+    print(' Ты очень хороший танкист')
+elif answer6 == 'д':
+    print("Иди в танки поиграй!")
+elif answer6 == 'б':
+    print("Иди в танки поиграй!")
+elif answer6 == 'в':
+    print("Иди в танки поиграй!")
+elif answer6 == 'г':
+    print("Иди в танки поиграй!")
