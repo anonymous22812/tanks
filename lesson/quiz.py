4c005aab6a47fffbec29d6e312862218b942ab5b
User <lesu_gtj2@student.itstep.org>
2022-12-12 08:46:08 +0200

    добавил новые вопросы

--- lesson/quiz.py
+++ lesson/quiz.py
@@ -40,7 +40,7 @@ elif answer2 == 'в':
 elif answer2 == 'г':
     print("Иди в танки поиграй!")
 
-print('Курица это-')
+print('Гриль это-')
 print('а) Grille 15')
 print('б) КВ-1С')
 print('в) ЛТГ')
@@ -50,7 +50,7 @@ print('д) M5 Stuart')
 answer3 = input('Ответ: ')
 
 if answer3 == 'а':
-    print(' Ты хороший танкист')
+    print(' Ты норм танкист')
 elif answer3 == 'д':
     print("Иди в танки поиграй!")
 elif answer3 == 'б':
@@ -74,7 +74,7 @@ answer4 = input('Ответ: ')
 if answer4 == 'в':
     print(' Ты хороший танкист')
 elif answer4 == 'д':
-    print("Нуб! Сосиса это 6-ка донатная")
+    print("")
 elif answer4 == 'б':
     print("Иди в танки поиграй!")
 elif answer4 == 'а':
@@ -200,6 +200,72 @@ print('б) II уровень')
 print('в) V уровень')
 print('г) VIII уровень')
 print('д) VI уровень')
+
+answer6 = input('Ответ: ')
+
+if answer6 == 'д':
+    print(' Ты хороший танкист')
+elif answer6 == 'г':
+    print("Иди в танки поиграй!")
+elif answer6 == 'б':
+    print("Иди в танки поиграй!")
+elif answer6 == 'в':
+    print("Иди в танки поиграй!")
+elif answer6 == 'а':
+    print("Иди в танки поиграй!")
+
+print('Какая самая последняя добавленая прокачеваемая десятка?')
+print('а) Foch 155')
+print('б) WZ-113b')
+print('в) Sheridan')
+print('г) Minotauro')
+print('д) Vickers Light')
+
+answer6 = input('Ответ: ')
+
+if answer6 == 'г':
+    print(' Ты хороший танкист')
+elif answer6 == 'д':
+    print("Иди в танки поиграй!")
+elif answer6 == 'б':
+    print("Иди в танки поиграй!")
+elif answer6 == 'в':
+    print("Иди в танки поиграй!")
+elif answer6 == 'а':
+    print("Иди в танки поиграй!")
+
+
+print('Твоя любимая десятка')
+print('а) T110E4')
+print('б) T110E3')
+print('в) T110E5')
+print('г) M-VI-Yoh')
+print('д) M48 Patton')
+print('е) T57 Heavy')
+print('ё) Sheridan')
+print('ж) Grille 15')
+print('з) Jg.Pz. E 100')
+print('и) VK 72.01 K')
+print('й) Maus')
+print('к) E 100')
+print('л) E 50 M')
+print('м) Leopard 1')
+print('н) Об. 268')
+print('о) Об. 263')
+print('п) ИС-7')
+print('р) ИС-4')
+print('с) Об. 140')
+print('т) Т-62А')
+print('у) Т-100 ЛТ')
+print('ф) FV215b 183')
+print('х) FV4005')
+print('ц) FV4002')
+print('ч) Vickers Light')
+print('ш) FV215b')
+print('щ) Ho-Ri')
+print('э) STB-1')
+print('ю) Type 71')
+print('я) В данном списке такого нет. Дальше ==>')
 
 answer6 = input('Ответ: ')
 
